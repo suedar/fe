f0bbd43676ab189c08bab38bf0c5ab642b8e1707
suedar <690372513@qq.com>
2020-04-26 23:26:59 +0800

    update

--- ans/algorithm/Backtracking/4.js
+++ ans/algorithm/Backtracking/4.js
@@ -3,5 +3,46 @@
  * @return {string[][]}
  */
 var partition = function(s) {
-    
+    let res = [];
+    if (s.length === 0) {
+        return res;
+    }
+    findPalidrome(0, s, res, []);
+    return res;
 };
+
+
+function findPalidrome(start, s, res, curArr) {
+    if (start === s.length) {
+        res.push(curArr);
+        return;
+    }
+
+    for (let i = start; i < s.length; i++) {
+        let subStr = s.slice(start, i + 1);
+        if (subStr && isPal(subStr)) {
+            findPalidrome(i + 1, s, res, [...curArr, subStr]);
+        }
+    }
+}
+
+
+function isPal(str) {
+    let len = Math.floor(str.length / 2);
+    if (len === 0) {
+        return true;
+    }
+    let add = str.length % 2 === 0 ? 0 : 1;
+    let subStr = str.slice(0, len);
+    for (let i = 0; i < len; i++) {
+        if (subStr[len - i - 1] !== str[len + add + i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// console.log(partition('aab'));
+// console.log(partition('12122'));
+// console.log(isPal('1111'))
+// console.log(isPal(''))
